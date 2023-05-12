298f3f66c97486bdb60b54b5b793c417ba78cd74
Santiago <santi.ponsle@gmail.com>
2023-05-12 19:08:40 -0300

    Fix linter errors

--- morse.rb
+++ morse.rb
@@ -1,46 +1,24 @@
 class MorseCodeDecoder
-  MORSE_CODE = {
-    '.-' => 'A', 
-    '-...' => 'B',
-    '-.-.' => 'C', 
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-     '....' => 'H',
-    '..' => 'I',
-     '.---' => 'J',
-    '-.-' => 'K',
-     '.-..' => 'L',
-    '--' => 'M',
-     '-.' => 'N',
-    '---' => 'O', 
-    '.--.' => 'P',
-    '--.-' => 'Q', 
-    '.-.' => 'R',
-    '...' => 'S', 
-    '-' => 'T',
-    '..-' => 'U', 
-    '...-' => 'V',
-    '.--' => 'W', 
-    '-..-' => 'X',
-    '-.--' => 'Y', 
-    '--..' => 'Z'
-} 
+  def self.decode_char(morse_char)
+    morse_code = {
+      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
+      '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
+      '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
+      '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+      '-.--' => 'Y', '--..' => 'Z'
+    }
+    morse_code[morse_char]
+  end
 
-def self.decode_char(morse_char)
-  MORSE_CODE[morse_char]
-end
+  def self.decode_word(morse_word)
+    morse_char = morse_word.split
+    morse_char.map { |char| decode_char(char) }.join
+  end
 
-def self.decode_word(morse_word)
-  morse_char = morse_word.split(' ')
-  morse_char.map {|morse_char| decode_char(morse_char)}.join('')
-end
-
-def self.message(message)
+  def self.message(message)
     morse_words = message.split('  ')
-    morse_words.map {|morse_word| decode_word(morse_word)}.join(' ')
-end
+    morse_words.map { |morse_word| decode_word(morse_word) }.join(' ')
+  end
 end
 
 message = MorseCodeDecoder.message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
